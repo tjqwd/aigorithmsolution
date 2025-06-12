2bc9b3415e04dc238cd3543544e4c68055727897
tjqwd <109289451+tjqwd@users.noreply.github.com>
2025-06-12 10:18:46 +0800

    Add files via upload

---
+++ algorithm/algorithm.java
@@ -0,0 +1,65 @@
+// 定义链表节点类
+class ListNode {
+              int val;
+              ListNode next;
+              ListNode(int x) { val = x; }
+          }
+          
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = l1, p2 = l2;
+        
+        while (p1 != null && p2 != null) {
+            // 比较 p1 和 p2 两个指针
+            // 将值较小的的节点接到 p 指针
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+        
+        if (p1 != null) {
+            p.next = p1;
+        }
+        
+        if (p2 != null) {
+            p.next = p2;
+        }
+        
+        return dummy.next;
+    }
+    // 辅助方法：打印链表
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // 创建第一个链表 1 -> 2 -> 4
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        // 创建第二个链表 1 -> 3 -> 4
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        Solution solution = new Solution();
+        ListNode mergedList = solution.mergeTwoLists(l1, l2);
+
+        // 打印合并后的链表
+        printList(mergedList);
+    }
+}
